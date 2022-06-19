c8c602b138a7ae22eb1545047cf51a573edc96ef
Eashika <85343520+Unique425@users.noreply.github.com>
2022-06-19 20:59:21 +0530

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,11 @@
+print("Welcome To Dictionary")
+dict = {
+    "Clique" : "A small close-knit group of people who do not readily allow others to join them.",
+    "Recidivism" : "The tendency of a convicted criminal to reoffend.",
+    "Ostracise " : "Exclude from a society or group.",
+    "Equivocation " : "The use of ambiguous language to conceal the truth or to avoid committing oneself",
+    "Consanguineous" : "Relating to or denoting people descended from the same ancestor.",
+}
+Search = input()
+print(dict[Search])
+print("Thanks For Using Dictionary")
